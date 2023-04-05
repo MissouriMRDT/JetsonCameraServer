9cb0e70038edce744bd6ff878f4583476d7e3698
ThisIsBrady <30638248+ThisIsBrady@users.noreply.github.com>
2023-04-05 00:12:42 -0500

    rovecomm works

--- Camera.py
+++ Camera.py
@@ -2,8 +2,12 @@ import threading
 import jetson_utils
 from jetson_utils import gstCamera
 import time
+from rovecomm.rovecomm import RoveComm, RoveCommPacket, get_manifest
 
 from typing import List, Dict, Tuple
+
+rovecomm_node: RoveComm
+manifest = get_manifest()
 
 class VideoDevice:
     device: int
@@ -30,12 +34,12 @@ class VideoDevice:
             # except Exception:
             #     self.video_source = None
             #     print(f"Error creating stream for device {self.device}")
-            self.output_by_endpoint[endpoint] = jetson_utils.video_source(f"rtp://{endpoint}", argv=['--width=640', '--height=480', '--headless', '--bitrate=1200000'])
+            self.output_by_endpoint[endpoint] = jetson_utils.video_source(f"{endpoint}", argv=['--width=640', '--height=480', '--headless', '--bitrate=1200000'])
         else:
             try:
                 self.video_source = jetson_utils.videoSource(f"/dev/video{self.device}", argv=['--width=640', '--height=480', '--headless', '--bitrate=1200000'])
                 self.video_source.Open()
-                self.output_by_endpoint[endpoint] = jetson_utils.videoOutput(f"rtp://{endpoint}", argv=['--width=640', '--height=480', '--headless', '--bitrate=1200000'])
+                self.output_by_endpoint[endpoint] = jetson_utils.videoOutput(f"{endpoint}", argv=['--width=640', '--height=480', '--headless', '--bitrate=1200000'])
             except Exception as e:
                 self.video_source = None
                 print(f"Error creating stream for device {self.device}")
@@ -50,18 +54,29 @@ class VideoDevice:
             return False
         else:
             assert False, "Invalid state in is_streaming"
-        
+    
+    def remove_endpoint(self, endpoint: str):
+        print("aaaaaaaaaaaaaaa")
+        assert endpoint in self.output_by_endpoint.keys()
+        print(self.output_by_endpoint.keys())
+        print("ssssssssssssssssss")
+        del self.output_by_endpoint[endpoint]
+        print(self.output_by_endpoint.keys())
+        if len(self.output_by_endpoint) == 0:
+            self.video_source = None
 
 class StreamManager:
     _lock = threading.Lock
 
+    _streams: List[int]
     _endpoints: List[str]
     _video_devices: List[VideoDevice]
     _device_endpoints: List[Tuple[int, int]]
 
     def __init__(self):
         self._video_devices = []
-        for i in range(2):
+        self._streams = [-1, -1, -1, -1]
+        for i in range(8):
             self._video_devices.append(VideoDevice(i * 4))
         self._endpoints = [
             "rtp://192.168.1.69:5000",
@@ -74,24 +89,55 @@ class StreamManager:
     def updateStreams(self):
         with self._lock:
             for index, video_dev in enumerate(self._video_devices):
-                try:
-                    #print(video_dev.device)
-                    image = video_dev.video_source.Capture(timeout=5000)
-                    for output in video_dev.output_by_endpoint.values():
-                        output.Render(image)
-                    # image.__delitem__()
-                except Exception as e: 
-                    # print("Error updating streams")
-                    # TODO: Stop streaming this device
-                    print(e)
-                    pass
+                if(video_dev.is_streaming()):
+                    try:
+                        #print(video_dev.device)
+                        image = video_dev.video_source.Capture(timeout=5000)
+                        for output in video_dev.output_by_endpoint.values():
+                            output.Render(image)
+                        # image.__delitem__()
+                    except Exception as e: 
+                        # print("Error updating streams")
+                        # TODO: Stop streaming this device
+                        print(e)
+                        pass
+
+    def change_feeds(self, packet):
+        with self._lock:
+            print("hello")
+            port, new_dev = packet.data
+            endpoint = self._endpoints[port]
+            prev_dev = self._streams[port]
+            
+            print(f"{port}:{new_dev}")
+
+            if prev_dev == new_dev:
+                #Do nothing, request is already being served
+                return
+            elif new_dev == -1:
+                print("none")
+                self._video_devices[prev_dev].remove_endpoint(endpoint)
+                self._streams[port] = -1
+            else:
+                if prev_dev == -1:
+                    print("new")
+                    self._video_devices[new_dev].create_stream(endpoint)
+                    self._streams[port] = new_dev
+                else:
+                    print("change")
+                    self._streams[port] = new_dev
+                    self._video_devices[prev_dev].remove_endpoint(endpoint)
+                    self._video_devices[new_dev].create_stream(endpoint)
+
 
 
 def main():
     streaming_manager = StreamManager()
-    streaming_manager._video_devices[0].create_stream("192.168.1.69:5000")
-    streaming_manager._video_devices[1].create_stream("192.168.1.69:5001")
+    # streaming_manager._video_devices[0].create_stream("192.168.1.69:5000")
+    # streaming_manager._video_devices[1].create_stream("192.168.1.69:5001")
 
+    rovecomm_node = RoveComm()
+    rovecomm_node.set_callback(manifest["Camera1"]["Commands"]["ChangeCameras"]["dataId"], streaming_manager.change_feeds)
     fps = 30
     loop_delta = 1./fps
 
